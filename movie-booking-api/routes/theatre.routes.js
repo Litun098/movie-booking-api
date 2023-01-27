83d8b5cba1ecee80d37108176f32ab833630f849
Litun098 <nayaklitun9@gmail.com>
2023-01-27 20:54:47 +0530

    Added theatre apis

--- movie-booking-api/routes/theatre.routes.js
+++ movie-booking-api/routes/theatre.routes.js
@@ -3,7 +3,7 @@ const theatreController = require("../controllers/theatre.controller");
 module.exports = function (app) {
 
     app.post("/mba/api/v1/theatres", theatreController.createTheatre);
-    app.get("/mba/api/v1/theatres", theatreController.getAllTheatres);
+    app.get("/mba/api/v1/theatres", theatreController.getAllTheatre);
     app.get("/mba/api/v1/theatres/:id", theatreController.getTheatre);
     app.put("/mba/api/v1/theatres/:id", theatreController.updateTheatre);
     app.delete("/mba/api/v1/theatres/:id", theatreController.deleteTheatre);
